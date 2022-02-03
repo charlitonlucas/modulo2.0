617303578e460b7e13fc354ccf6d27a559abc131
charlitonlucas <charlitonlessa@gmail.com>
2022-02-03 19:20:11 -0300

    first commit

---
+++ senacflix.js
@@ -0,0 +1,10 @@
+console.log(" Usuário 1: assistindo filme");
+console.log(" Usuário 2: assistindo série");
+console.log(" Usuário 3: procurando filme");
+console.log("usuário 4 assistindo novela");
+
+setTimeout(function() {
+
+    console.log("primeiro");
+    }, 0);
+    console.log("segundo");
