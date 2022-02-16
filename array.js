4490c3566f750a1d3a0816da51f34e840c2830db
charlitonlucas <charlitonlessa@gmail.com>
2022-02-15 21:17:13 -0300

    array

---
+++ array.js
@@ -0,0 +1,27 @@
+const lista = new Array("pera","uva","maçã","salada");
+const numeros = [9,2,5,7,8,9]
+
+
+
+const cadastro= [
+                 {nome: "Paula" , matricula: "0002" , curso: "web back end" },
+                 {nome: "Pedro" , matricula: "0003" , curso: "análise de sistemas"},
+                 {nome: "Silvia", matricula: "0004" , curso: "ADS"}
+                ]
+
+
+
+const prieiroitem= lista[2]
+const tamanho= lista.length
+const tamanho2= numeros.length
+const tamanho3= cadastro.length
+const informacoes=cadastro[0];
+
+
+console.log(tamanho2,tamanho);
+console.log(cadastro);
+console.log(tamanho3);
+console.log(informacoes);
+
+console.log(cadastro[2].nome);
+
