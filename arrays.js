0cb00d81a312d81f83b4556a271144d373fa29a2
Charliton Lucas <93688266+charlitonlucas@users.noreply.github.com>
2022-02-17 20:12:10 -0300

    Add files via upload

---
+++ arrays.js
@@ -0,0 +1,9 @@
+const numeros = [10, 11, 5, 7];
+
+const alunos = [
+  { nome: "carlos", profissao: "vendedor", idade: "50" },
+  { nome: "luana", profissao: "vendedora", idade: "20" },
+  { nome: "laura", profissao: "medica", idade: "35" },
+];
+
+console.log(alunos[1].idade);
